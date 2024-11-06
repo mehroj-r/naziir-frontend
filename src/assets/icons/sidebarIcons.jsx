e2c9df3d2497fac81fee77a1b8a3988d18274b42
NurdauletShinpolatov <nshinpolatov@gmail.com>
2024-11-07 01:00:07 +0500

    added new layout

---
+++ src/assets/icons/sidebarIcons.jsx
@@ -0,0 +1,73 @@
+export function HomeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="white"
+    >
+      <path d="M12.71 2.29a1 1 0 0 0-1.42 0l-9 9a1 1 0 0 0 0 1.42A1 1 0 0 0 3 13h1v7a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7h1a1 1 0 0 0 1-1 1 1 0 0 0-.29-.71zM6 20v-9.59l6-6 6 6V20z"></path>
+    </svg>
+  );
+}
+
+export function CourseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="white"
+    >
+      <path d="M19 2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19s.55-.988 1.012-1H21V4c0-1.103-.897-2-2-2zm0 14H5V5c0-.806.55-.988 1-1h13v12z"></path>
+    </svg>
+  );
+}
+
+export function CreateQuizIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="white"
+    >
+      <path d="m21.484 11.125-9.022-5a1 1 0 0 0-.968-.001l-8.978 4.96a1 1 0 0 0-.003 1.749l9.022 5.04a.995.995 0 0 0 .973.001l8.978-5a1 1 0 0 0-.002-1.749zm-9.461 4.73-6.964-3.89 6.917-3.822 6.964 3.859-6.917 3.853z"></path>
+      <path d="M12 22a.994.994 0 0 0 .485-.126l9-5-.971-1.748L12 19.856l-8.515-4.73-.971 1.748 9 5A1 1 0 0 0 12 22zm8-20h-2v2h-2v2h2v2h2V6h2V4h-2z"></path>
+    </svg>
+  );
+}
+
+export function QuizzesIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="white"
+    >
+      <path d="M22 7.999a1 1 0 0 0-.516-.874l-9.022-5a1.003 1.003 0 0 0-.968 0l-8.978 4.96a1 1 0 0 0-.003 1.748l9.022 5.04a.995.995 0 0 0 .973.001l8.978-5A1 1 0 0 0 22 7.999zm-9.977 3.855L5.06 7.965l6.917-3.822 6.964 3.859-6.918 3.852z"></path>
+      <path d="M20.515 11.126 12 15.856l-8.515-4.73-.971 1.748 9 5a1 1 0 0 0 .971 0l9-5-.97-1.748z"></path>
+      <path d="M20.515 15.126 12 19.856l-8.515-4.73-.971 1.748 9 5a1 1 0 0 0 .971 0l9-5-.97-1.748z"></path>
+    </svg>
+  );
+}
+
+export function StatisticsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="white"
+    >
+      <path d="M2 20h20v2H2zM4 3v14a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1zm2 1h3v12H6zM13 17a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-5a1 1 0 0 0-1 1zm2-9h3v8h-3z"></path>
+    </svg>
+  );
+}
+
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,73 @@
+import styles from "./Sidebar.module.scss";
+import logo from "../../assets/images/whiteLogo.png";
+import newuuLogo from "../../assets/images/newuuLogo.png";
+import {
+  CourseIcon,
+  CreateQuizIcon,
+  HomeIcon,
+  QuizzesIcon,
+  StatisticsIcon,
+} from "../../assets/icons/sidebarIcons";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const items = [
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: <HomeIcon />,
+    navigateTo: "/",
+  },
+  {
+    id: 2,
+    title: "Mu Courses",
+    icon: <CourseIcon />,
+    navigateTo: "/courses",
+  },
+  {
+    id: 3,
+    title: "Create Quiz",
+    icon: <CreateQuizIcon />,
+    navigateTo: "/create-quiz",
+  },
+  {
+    id: 4,
+    title: "My Quizzes",
+    icon: <QuizzesIcon />,
+    navigateTo: "/quizzes",
+  },
+  {
+    id: 5,
+    title: "Statistics",
+    icon: <StatisticsIcon />,
+    navigateTo: "/statistics",
+  },
+];
+
+export default function Sidebar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.logos}>
+        <img src={logo} alt="Naziir-logo" className={styles.naziirLogo} />
+        <img src={newuuLogo} alt="NewUU-logo" className={styles.newuuLogo} />
+      </div>
+      <div className={styles.pages}>
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className={`
+                ${styles.item} 
+                ${location?.pathname == item.navigateTo ? styles.active : ''}
+            `}
+            onClick={() => navigate(item.navigateTo)}
+          >
+            {item.icon}
+            <span>{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
